65791db44085de84d33a5bb4e5a6160da37e2164
jespy3 <joshua.espinas@gmail.com>
2020-08-22 12:51:53 +1200

    Removes dockerfile and reference for dev environment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const env = process.env.DEPLOY_ENVIRONMENT || "dev"
 
-app.get('/', (req, res) => res.send(`Hello World! I\'m in the ${env} environment!!!`))
+app.get('/', (req, res) => res.send(`Hello World! This is for SOT`))
 app.get('/ping', (req, res) => res.send('pong'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
