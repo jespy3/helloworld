ec63c6f57bc1b3777dad82220e50a6289cb6339b
jespy3 <joshua.espinas@gmail.com>
2020-08-22 12:43:30 +1200

    Allows port to be accessed by heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const env = process.env.DEPLOY_ENVIRONMENT || "dev"
 
 app.get('/', (req, res) => res.send(`Hello World! I\'m in the ${env} environment!!!`))
